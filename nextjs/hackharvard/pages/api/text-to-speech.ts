aad08931e8b31b6b6f836e21b240e860fa7d1135
David Prelinger <53475909+David-Prelinger@users.noreply.github.com>
2023-10-21 12:48:13 -0400

    Updated text-to-speech

--- nextjs/hackharvard/pages/api/text-to-speech.ts
+++ nextjs/hackharvard/pages/api/text-to-speech.ts
@@ -1,9 +1,58 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    
+    // Ensure the request is a POST request
+    if (req.method !== 'POST') {
+        res.status(405).send('Method Not Allowed'); // Return a 405 Method Not Allowed if it's not a POST request
+        return;
+    }
+
+    // Ensure the request has a JSON body
+    if (!req.body) {
+        res.status(400).send('Bad Request'); // Return a 400 Bad Request if there's no body
+        return;
+    }
+
+    const { voiceName: voiceId, text } = req.body; // Destructure the voiceName and text from the request body
+
+    if (!voiceId || !text) {
+        res.status(400).send('Bad Request'); // Return a 400 Bad Request if either voiceName or text are missing
+        return;
+    }
+
+    const apiKey = 'c9a4a0e489737ef5f7f7f12b2c1d6df4';
+    const voiceSettings = { stability: 1, similarity_boost: 0 };
+
+    const baseUrl = "https://api.elevenlabs.io/v1/text-to-speech";
+    const headers = {
+        "Content-Type": "application/json",
+        "xi-api-key": apiKey,
+    };
+
+    const requestBody = {
+        text,
+        voice_settings: voiceSettings,
+    };
+
+    try {
+        const response = await axios.post(`${baseUrl}/${voiceId}`, requestBody, {
+            headers,
+            responseType: "arraybuffer",  // Change to arraybuffer to handle binary data
+        });
+
+        if (response.status === 200) {
+            res.setHeader('Content-Type', 'audio/wav');  // Assuming the audio is in wav format
+            res.status(200).send(response.data);
+        } else {
+            res.status(500).send('External API request failed');
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('An error occurred');
+    }
 }
